71dc47933d17362b7bc132fed55f53ba4d48faac
Lucas Loz <lucass.mark@gmail.com>
2023-11-10 18:39:54 -0300

    docs: create change log and change the project version to 0.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.example.taskapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.taskapp"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
